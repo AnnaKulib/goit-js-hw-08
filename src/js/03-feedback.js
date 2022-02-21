f1bd99c7c2a7f83231976d77defddf3c9e7fb5b8
AnnaKulib <83002978+AnnaKulib@users.noreply.github.com>
2022-02-21 12:56:01 +0200

    add task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
 
+const throttle = require('lodash.throttle');
+const KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const saveData = localStorage.getItem(KEY);
+const saveDataParsed = JSON.parse(saveData);
+
+if (saveDataParsed) {
+    form['email'].value = savedDataParsed.email;
+    form['message'].value = savedDataParsed.message;
+}
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+    const formData = { email: `${form["email"].value}`, message: `${form["message"].value}` };
+    const formDataSaved = JSON.stringify(formData);
+
+    localStorage.setItem(KEY, formDataSaved);
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+    evt.preventDefault();
+    const formData = { email: `${form['email'].value}`, message: `${form['message'].value}` };
+    console.log(formData);
+
+    if (formData.email === '' || formData.message === '') {
+        return alert("Заполните все поля!");
+    }
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(KEY);
+}
